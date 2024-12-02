75f826451038ceb0dc9fa8d0381e4173ae791926
Henri Lepic <henrilepic@gmail.com>
2024-12-02 11:54:53 +0100

    add

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
 
 	ser "github.com/dgkg/banana/services"
 )
@@ -9,8 +11,8 @@ import (
 const StatusCodeOk = 200
 
 const (
-	StatusCodeCreated   restStatus = 201
-	StatusCodeNoContent restStatus = 204
+	StatusCodeCreated restStatus = iota + 201
+	StatusCodeNoContent
 )
 
 type restStatus int
@@ -34,6 +36,11 @@ func initApp() {
 }
 
 func main() {
+	ReadFile()
+	// execCode()
+}
+
+func execCode() error {
 	fmt.Println(ser.Add(1, 2))
 
 	var u myString = "user"
@@ -41,7 +48,7 @@ func main() {
 
 	res, err := ser.Sub("1", "2")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 	fmt.Println(res)
 	f := fibo()
@@ -52,11 +59,39 @@ func main() {
 	if res := ser.Add(1, 2); res != 3 {
 		fmt.Println(err)
 		err = generateError()
+		if err != nil {
+			return err
+		}
 	}
 
 	fmt.Printf("%T - %v\n", StatusCodeOk, StatusCodeOk)
 	fmt.Printf("%T - %v\n", StatusCodeCreated, StatusCodeCreated)
 	fmt.Printf("%T - %v\n", StatusCodeNoContent, StatusCodeNoContent)
+
+	var s string
+	fmt.Printf("%T - `%v`\n", s, s)
+	var i int
+	fmt.Printf("%T - %v\n", i, i)
+
+	m := make(map[string]int)
+	m["toto"] = 1
+	val, ok := m["toto"]
+	fmt.Printf("%T - %v - %v\n", m, val, ok)
+	delete(m, "toto")
+	val, ok = m["toto"]
+	fmt.Printf("%T - %v - %v\n", m, val, ok)
+
+	mCal.Add(1, 2)
+	mCal.Sub("1", "2")
+
+	var usr User
+	usr.id = 1
+	usr.Name = "toto"
+	usr.BirthDate = time.Now()
+	usr.CreatedAt = time.Now()
+	usr.UpdatedAt = time.Now()
+
+	return nil
 }
 
 func generateError() error {
@@ -71,4 +106,36 @@ func fibo() func() int {
 		a, b = b, a+b
 		return a
 	}
+}
+
+func ReadFile() error {
+	defer fmt.Println("defer")
+	data, err := os.ReadFile("toto.json")
+	if err != nil {
+		return err
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println(string(data))
+	return nil
+}
+
+type myCalculator = ser.Calculator
+
+var mCal myCalculator
+
+type User struct {
+	id   int
+	Name string
+	Human
+	BddFields
+}
+
+type Human struct {
+	BirthDate time.Time
+}
+
+type BddFields struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeleteAt  *time.Time
 }
--- services/service.go
+++ services/service.go
@@ -19,3 +19,21 @@ func Sub(a, b string) (int, error) {
 	}
 	return resa - resb, nil
 }
+
+type Calculator int
+
+func (c *Calculator) Add(a, b int) int {
+	return a + b
+}
+
+func (c *Calculator) Sub(a, b string) (int, error) {
+	resa, err := Atoi(a)
+	if err != nil {
+		return 0, err
+	}
+	resb, err := Atoi(b)
+	if err != nil {
+		return 0, err
+	}
+	return resa - resb, nil
+}
