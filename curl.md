a60837fc6afe8d9729d74adf38e7abe30ea81d85
Henri Lepic <henrilepic@gmail.com>
2024-12-04 14:29:08 +0100

    init gin

---
+++ curl.md
@@ -0,0 +1,7 @@
+## User
+
+### register
+
+```sh
+curl -X POST http://localhost:8000/register -H 'Content-Type: application/json' -d '{"name":"Henri", "pass":"mysuperpass", "email":"email@toto.com"}'
+```
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("toto")
+	r := gin.Default()
+	r.POST("/register", HandleRegister)
+	r.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+type User struct {
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"pass" form:"pass"`
+}
+
+func HandleRegister(ctx *gin.Context) {
+	var payload User
+	err := ctx.Bind(&payload)
+	if err != nil {
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	ctx.JSON(http.StatusOK, payload)
 }
